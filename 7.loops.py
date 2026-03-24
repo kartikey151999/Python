b48321f4feb42f54abaed9d937cf38b93dbbaee5
Kartikey Pande <kartikey.pande@cleanstart.com>
2026-03-24 15:46:19 +0530

    loops

---
+++ 7.loops.py
@@ -0,0 +1,16 @@
+sentence = "online python compiler"
+
+for index, character in enumerate(sentence):
+    if character == " ":
+        print(index, "#")
+    else:
+        print(index, character)
+
+
+x = 10
+while x > -1:
+    print(x)
+    x -= 1
+
+# break - will comeout from the loop
+# continue - skip the sentence after the continue an
